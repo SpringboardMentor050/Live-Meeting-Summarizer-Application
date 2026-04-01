75d3b4aaeb24ee598441ea09f6b552e6e8bd3c3f
NathanAryan <nathanaryan12@gmail.com>
2026-04-02 02:36:48 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import tempfile
-
-def save_uploaded_file(uploaded_file):
-    
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        temp_file.write(uploaded_file.read())
-        temp_path = temp_file.name
-
-    return temp_path
